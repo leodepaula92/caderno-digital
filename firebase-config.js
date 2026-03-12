9c413eeda6017cef19c4494da2c2593941552634
leodepaula92 <leodepaula@outlook.pt>
2026-03-12 13:49:47 -0300

    Add Firebase configuration to firebase-config.js
    Added Firebase configuration settings for the project.

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,10 @@
-
+// Configurações do seu Firebase
+export const firebaseConfig = {
+    apiKey: "AIzaSyAcQ-eIuJLxLCelWXb40BBWSo6OlndAWoY",
+    authDomain: "meu-caderno-digital.firebaseapp.com",
+    databaseURL: "https://meu-caderno-digital-default-rtdb.firebaseio.com",
+    projectId: "meu-caderno-digital",
+    storageBucket: "meu-caderno-digital.firebasestorage.app",
+    messagingSenderId: "300654804825",
+    appId: "1:300654804825:web:4272fc4c67be0b485d1648"
+};
